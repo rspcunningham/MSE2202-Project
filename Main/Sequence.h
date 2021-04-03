e97752586584e42f33add25ae7a8c528040c1ac3
rspcunningham <79066805+rspcunningham@users.noreply.github.com>
2021-04-03 12:36:05 -0400

    timer

--- Main/Sequence.h
+++ Main/Sequence.h
@@ -1,28 +1,30 @@
-void moveRobotSequence()
-{
+void moveRobotSequence() {
     int targetAngle;
-    switch (robotDriveSequence)
-    {
-    case 0://Drive forward until edge of box
-        targetAngle = 0; //Drive forward
-        runMotors(targetAngle); //Run motors forward
-        Serial.println(getDistance());
-        if(getDistance()>=500){ //Does the US has a large distance input
-            Serial.println("YESSSSSSSSSS");
-            double pastBoxTimer=millis(); //Start timer for how long US has had a large distance
-            if((millis()-pastBoxTimer)>=100){ //Has 100ms passed with the US having a large distance
-                robotDriveSequence++; //Go to next step in the drive sequence
+
+    long currentTime = millis();
+    static long lastTime;
+
+    switch (robotDriveSequence) {
+        case 0:                      //Drive forward until edge of box
+            targetAngle = 0;         //Drive forward
+            runMotors(targetAngle);  //Run motors forward
+            Serial.println(getDistance());
+            if (getDistance() >= 500) {  //Does the US has a large distance input
+                Serial.println("YESSSSSSSSSS");
+
+                if ((currentTime - lastTime) >= 100) {  //Has 100ms passed with the US having a large distance
+                    robotDriveSequence++;               //Go to next step in the drive sequence
+                }
             }
-        }
-        break;
-    case 1:
-        stopMotors();
-        if((millis()-stopTimer)>=200){
-            robotDriveSequence++;
-        }
-        break;
-    case 2:
-        //runRightMotor
-        break;
+            break;
+        case 1:
+            stopMotors();
+            if ((millis() - stopTimer) >= 200) {
+                robotDriveSequence++;
+            }
+            break;
+        case 2:
+            //runRightMotor
+            break;
     }
 }
